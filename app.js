70f3b5cc03e8c07b61b920514edc228285192d17
Parveenrps <pk525410@gmail.com>
2026-04-02 20:50:58 +0530

    error handling

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 import express from 'express'
-// import cors from 'cors'
+import { errorHandler } from './middlewares/error.middleware.js';
+import cors from 'cors'
 const app = express();
 
 app.use(express.json());
-// app.use(cors())
+app.use(errorHandler)
+app.use(cors())
 
 import authRoutes from './routes/auth.routes.js';
 import userRoutes from './routes/user.routes.js';
 import recordRoutes from './routes/record.routes.js';
 import dashboardRoutes from './routes/dashboard.routes.js';
+
 
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
